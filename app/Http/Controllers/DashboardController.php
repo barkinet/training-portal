d08546c236953c0e5b0ab5ebb14700c679d19bb2
Cameron Pavey <cpave3@gmail.com>
2016-12-13 10:26:45 +1000

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //Controls the general dashboard
+
+    public function index() {
+      //First thing the user sees when they log in
+
+    }
+
+}
